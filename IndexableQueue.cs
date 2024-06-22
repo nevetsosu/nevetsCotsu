90fb5c4ff28f2b707dbde4f75991bf90e8a7b8b9
Steven Dao <stevendao100@gmail.com>
2024-06-21 19:49:36 -0500

    more functions in the Indexable Queue

--- IndexableQueue.cs
+++ IndexableQueue.cs
@@ -50,12 +50,72 @@ public class IndexableQueue<T> {
 
      }
 
-     private static void Remove(IndexableQueueNode<T> node) {
+     public static void Remove(IndexableQueueNode<T> node) {
           IndexableQueueNode<T> afterNode = node.next!;
           IndexableQueueNode<T> beforeNode = node.next!;
 
           beforeNode.next = afterNode;
           afterNode.prev = beforeNode;
+     }
+
+     public IndexableQueueNode<T>? Remove(int index) {
+          if (int.Abs(index) > Count) throw new ArgumentOutOfRangeException("Absolute value of the index should be less than or equal to Queue size");
+          IndexableQueueNode<T> node = GetNode(sentinel, index);
+          Remove(node);
+          return node;
+     }
+
+     // makes the node at index the next in the queue. this also skips over all nodes that were to the left of new node but right of the sentinel
+     public IndexableQueueNode<T> SkipTo(int index) {
+          IndexableQueueNode<T> newNode = GetNode(sentinel, index);
+
+          sentinel.next = newNode;
+          newNode.prev = sentinel;
+          return newNode;
+     }
+
+     public void Swap(int IndexA, int IndexB) {
+          Swap(GetNode(sentinel, IndexA), GetNode(sentinel, IndexB));
+     }
+
+     public void Swap(IndexableQueueNode<T> NodeA, IndexableQueueNode<T> NodeB) {
+          T TempData = NodeA.data;
+          NodeA.data = NodeB.data;
+          NodeB.data = TempData;
+     }
+
+     public IndexableQueueNode<T> Get(int index) {
+          if (int.Abs(index) > Count) throw new ArgumentOutOfRangeException("Absolute value of the index should be less than or equal to Queue size");
+          return GetNode(sentinel, index);
+     }
+
+     // will get node that is (index) positions to the right (when index is positive) or left (when index is negative)
+     private static IndexableQueueNode<T> GetNode(IndexableQueueNode<T> startNode, int index) {
+          if (int.IsPositive(index)) {
+               return GetNodeRight(startNode, index);
+          } else {
+               return GetNodeLeft(startNode, -index);
+          }
+     }
+
+     private static IndexableQueueNode<T> GetNodeLeft(IndexableQueueNode<T> startNode, int offset) {
+          IndexableQueueNode<T> node = startNode;
+
+          while (offset > 0) {
+               node = node.prev!;
+          }
+
+          return node;
+     }
+
+     private static IndexableQueueNode<T> GetNodeRight(IndexableQueueNode<T> startNode, int offset) {
+          IndexableQueueNode<T> node = startNode;
+
+          while (offset > 0) {
+               node = node.next!;
+          }
+
+          return node;
      }
 
      public bool TryDequeue(out T item) {
